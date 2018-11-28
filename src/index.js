bd4be9b97cb9ab9d1dbc3fcd47f95dd1cc6423ba
richardmaccaw <ricci6@gmail.com>
2018-11-28 12:18:35 +0000

    changing how state works

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ const dogFilterBtn = document.querySelector('#good-dog-filter')
 const dogBarEl = document.querySelector('#dog-bar')
 const dogInfoEl = document.querySelector('#dog-info')
 
-const status = []
+let state = []
+
+
+
 
 // FETCH DOGS ON PAGE LOAD
 document.addEventListener('DOMContentLoaded', event => {
@@ -13,15 +16,70 @@ document.addEventListener('DOMContentLoaded', event => {
         .then(renderDogs)
 })
 
-// ADD DOG TO DOG BAR
+// RENDER DOG TO DOG BAR AND SUMMARY BAR
 const renderDog = (dog) => {
     const dogSpan = document.createElement('span')
     dogSpan.innerText = dog.name
     dogBarEl.appendChild(dogSpan)
-    status.push(dog)
+    state.push(dog)
+
+    dogSpan.addEventListener('click', () => {
+
+        const renderGoodOrBadDog = () => dog.isGoodDog ? 'Good Dog!' : 'Bad Dog!'
+        
+        dogInfoEl.innerHTML = `
+            <img src="${dog.image}" alt="Dog Image">
+            <h2>${dog.name}</h2>
+            <button id='dog-${dog.id}'>${renderGoodOrBadDog()}</button>
+        `
+        const dogBtn = dogInfoEl.querySelector('button')
+        dogBtn.addEventListener('click', event => {
+            dog.isGoodDog = !dog.isGoodDog
+            dogBtn.textContent = renderGoodOrBadDog()
+            patchDog(dog)
+                .catch(err => alert('Failed to write to database. Soz'))
+        })
+    })
 }
 
-// ADD DOGS
+// SORT DOGS
+dogFilterBtn.addEventListener('click', event => {
+    goodBoyFilter = false
+
+    const changeBtnText = () => {
+        return dogFilterBtn.innerText === 
+            'Filter good dogs: OFF' ?
+            'Filter good dogs: ON' : 'Filter good dogs: OFF'
+    }
+
+    const getGoodDogs = () => state.filter(function(dog) {
+        return dog.isGoodDog
+    })
+
+    goodBoyFilter = !goodBoyFilter
+    dogFilterBtn.innerText = changeBtnText()
+
+    if (goodBoyFilter) {
+        clearDogs()
+        renderDogs(getGoodDogs())
+    } else {
+        clearDogs()
+        renderDogs(state)
+    }
+
+})
+
+// CLEAR DOGS
+const clearDogs = () => {
+    dogBarEl.innerHTML = ''
+}
+
+// RENDER DOGS
 const renderDogs = (dogs) => {
     dogs.forEach(dog => renderDog(dog))
+}
+
+//UPDATE STATE
+const updateState = () => {
+
 }
